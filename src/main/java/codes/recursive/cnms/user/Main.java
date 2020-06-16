7738f8067841387b02efb68bd9ab0db150f5a01b
sasankapdn <sasanka.abeysinghe@oracle.com>
2020-06-16 14:44:52 -0500

    self sign cert addition

--- src/main/java/codes/recursive/cnms/user/Main.java
+++ src/main/java/codes/recursive/cnms/user/Main.java
@@ -18,13 +18,32 @@ package codes.recursive.cnms.user;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
 import java.util.logging.LogManager;
 
+import com.squareup.okhttp.OkHttpClient;
+import io.helidon.common.configurable.Resource;
+import io.helidon.common.pki.KeyConfig;
+import io.helidon.config.Config;
 import io.helidon.microprofile.server.Server;
+import io.netty.handler.ssl.ApplicationProtocolConfig;
+
+import javax.net.ssl.*;
+
 
 /**
  * The application main class.
  */
+
 public final class Main {
 
     /**
@@ -41,11 +60,20 @@ public final class Main {
     public static void main(final String[] args) throws IOException {
         // load logging configuration
         setupLogging();
+        try {
+            trustCert();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
 
-        // start the server
         Server server = startServer();
 
         System.out.println("http://localhost:" + server.port() + "/greet");
+        //SimpleHttpsServer httpsServer = new SimpleHttpsServer();
+        //httpsServer.Start(8080);
+        //System.out.println(httpsServer.getPath());
     }
 
     /**
@@ -56,7 +84,11 @@ public final class Main {
         // Server will automatically pick up configuration from
         // microprofile-config.properties
         // and Application classes annotated as @ApplicationScoped
+        //Config config = Config.create();
+        //return Server.builder().config(config).build().start();
         return Server.create().start();
+
+
     }
 
     /**
@@ -67,4 +99,44 @@ public final class Main {
             LogManager.getLogManager().readConfiguration(is);
         }
     }
+
+    private static void trustCert() throws NoSuchAlgorithmException, KeyManagementException, IOException {
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }
+        };
+
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        URL url = new URL("https://www.nakov.com:2083/");
+        URLConnection con = url.openConnection();
+        Reader reader = new InputStreamReader(con.getInputStream());
+        while (true) {
+            int ch = reader.read();
+            if (ch==-1) {
+                break;
+            }
+            System.out.print((char)ch);
+        }
+    }
+
 }
